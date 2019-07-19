777d9c21d90bdd8d4bba84f32786878476e5d1cf
tania <tanislavens@yahoo.com>
2019-07-19 15:10:29 -0400

    wip: char cards and content showing. min styling present

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -9,28 +9,33 @@ function App() {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [char, setChar] = useState([]);
+  const [error, setError] = useState('');
   useEffect(() => {
     axios.get('https://swapi.co/api/people/')
       .then(res => {
-        //console.log(res.data);
-        setChar(res.data);
+        // console.log(res.data);
+        setChar(res.data.results);
         // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
         // side effect in a component, you want to think about which state and/or props it should
         // sync up with, if any.
-      });
+      })
+      .catch(err => {
+        console.log('error occurred', error);
+      })
   }, [])
 
   return (
     <StyledContainer>
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        {char.map(data => (
-          <CharCard gender={data.gender}
-            age={data.birth_year}
-            height={data.height}
-            mass={data.mass}
-            hairColor={data.hair_color}
-            eyeColor={data.eye_color} />
+        {char.map(results => (
+          <CharCard name={results.name}
+            gender={results.gender}
+            age={results.birth_year}
+            height={results.height}
+            mass={results.mass}
+            hair={results.hair_color}
+            eyes={results.eye_color} />
         ))}
       </div>
     </StyledContainer>
--- starwars/src/components/Styled.js
+++ starwars/src/components/Styled.js
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 //styled container
 export const StyledContainer = styled.div`
-    background-color: tan;
     margin-left: auto;
     margin-right: auto;
-    max-width: 50%;
+    max-width: 30%;
     height: 800px;
 `;
